5e9385ae3821f1c462a76771918b219b62f453f7
Hwangtaehun <r283@naver.com>
2024-09-06 19:15:53 +0900

    Update Person.java

--- sq-ch3-7/src/main/java/beans/Person.java
+++ sq-ch3-7/src/main/java/beans/Person.java
@@ -1,12 +1,14 @@
 package beans;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Person {
     private String name = "Ella";
     private final Parrot parrot;
 
-    public Person(@Qualifier("parrot2") Parrot parrot){
+    public Person(@Qualifier("parrot2") Parrot parrot) {
         this.parrot = parrot;
     }
 
