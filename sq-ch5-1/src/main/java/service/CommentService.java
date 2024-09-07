cf72da9fc833e344e90a2335ad20a9d50a5f886f
Hwangtaehun <r283@naver.com>
2024-09-07 19:32:06 +0900

    업로드

---
+++ sq-ch5-1/src/main/java/service/CommentService.java
@@ -0,0 +1,20 @@
+package service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import repository.CommentRepository;
+
+@Service
+public class CommentService {
+
+    @Autowired
+    private final CommentRepository commentRepository;
+
+    public CommentService(CommentRepository commentRepository){
+        this.commentRepository = commentRepository;
+    }
+
+    public CommentRepository getCommentRepository() {
+        return commentRepository;
+    }
+}
