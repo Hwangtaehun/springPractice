92323774310140f3de0cd112073aa0512c907ce6
Hwangtaehun <r283@naver.com>
2024-09-11 14:32:39 +0900

    업로드

---
+++ sq-ch10-2/src/test/java/com/example/sq_ch10_1/SqCh101ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sq_ch10_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqCh101ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
