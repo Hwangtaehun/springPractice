cb411de0fce5cd9daf4f8d9bd839b228cc1db953
Hwangtaehun <r283@naver.com>
2024-09-12 11:12:50 +0900

    업로드

--- sq-ch11-1/src/main/java/com/example/proxy/PaymentsProxy.java
+++ sq-ch11-1/src/main/java/com/example/proxy/PaymentsProxy.java
@@ -6,8 +6,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name="payments", url="${name.service.url}")
-public interface PaymentProxy {
+@FeignClient(name = "payments", url = "${name.service.url}")
+public interface PaymentsProxy {
+
     @PostMapping("/payment")
-    Payment createPayment(@RequestHeader String requestId, @RequestBody Payment payment);
+    Payment createPayment(
+            @RequestHeader String requestId,
+            @RequestBody Payment payment);
+
 }
