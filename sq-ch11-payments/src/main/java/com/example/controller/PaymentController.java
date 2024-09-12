ec67d52a4c80bfa98cb3d65c34282c294eaa3600
Hwangtaehun <r283@naver.com>
2024-09-12 10:50:44 +0900

    업로드

---
+++ sq-ch11-payments/src/main/java/com/example/controller/PaymentController.java
@@ -0,0 +1,25 @@
+package com.example.controller;
+
+import com.example.model.Payment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+import java.util.UUID;
+import java.util.logging.Logger;
+
+public class PaymentController {
+    private static Logger logger = Logger.getLogger(PaymentController.class.getName());
+
+    @PostMapping("/payment")
+    public ResponseEntity<Payment> createPayment(
+            @RequestHeader String requestId,
+            @RequestBody Payment payment
+    ){
+        logger.info("Received request with ID " + requestId + " ;Payment Amount: " + payment.getAmount());
+        payment.setId(UUID.randomUUID().toString());
+        return ResponseEntity.status(HttpStatus.OK).header("requestId", requestId).body(payment);
+    }
+}
