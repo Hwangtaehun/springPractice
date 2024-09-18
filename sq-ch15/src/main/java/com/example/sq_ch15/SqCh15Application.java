d87bc99190e81f4dc0645877106d25879d7cd0f4
Hwangtaehun <r283@naver.com>
2024-09-18 17:56:25 +0900

    업로드

---
+++ sq-ch15/src/main/java/com/example/sq_ch15/SqCh15Application.java
@@ -0,0 +1,13 @@
+package com.example.sq_ch15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqCh15Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqCh15Application.class, args);
+	}
+
+}
---
+++ sq-ch15/src/test/java/com/example/sq_ch15/SqCh15ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sq_ch15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqCh15ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
