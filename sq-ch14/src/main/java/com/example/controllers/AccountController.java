f72aac2f4892d68a9079a45f640f9c9eee4d33f5
Hwangtaehun <r283@naver.com>
2024-09-19 20:56:51 +0900

    업로드

---
+++ sq-ch14/src/main/java/com/example/controllers/AccountController.java
@@ -0,0 +1,30 @@
+package com.example.controllers;
+
+import com.example.dto.TransferRequest;
+import com.example.model.Account;
+import com.example.services.TransferService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public class AccountController {
+    private final TransferService transferService;
+    public AccountController(TransferService transferService) {
+        this.transferService = transferService;
+    }
+
+    @PostMapping("/transfer")
+    public void transferMoney(@RequestBody TransferRequest request) {
+        transferService.transferMoney(request.getSenderAccountId(), request.getReceiverAccountId(), request.getAmount());
+    }
+
+    @GetMapping("/accounts")
+    public Iterable<Account> getAllAccounts(@RequestParam(required = false) String name) {
+        if(name == null) {
+            return transferService.getAllAccounts();
+        } else {
+            return transferService.findAccountsByName(name);
+        }
+    }
+}
